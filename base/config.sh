e39bcd0920035ac434655c97ff79b0611ef88e8e
Vedarth <vedarth.sharma@gmail.com>
2019-05-28 01:51:50 +0530

    Adds repos correctly and removes reduntant code

--- base/config.sh
+++ base/config.sh
@@ -56,18 +56,14 @@ if [ -d /etc/zypp/vars ]; then
 fi
 
 zypper -c ${zypp_config} --installroot=${target} --releasever=/ \
-    --setopt=tsflags= \
-    --setopt=group_package_types=mandatory -y install \
     sles-release \
     SLE-Module-Basesystem15-Debuginfo-Pool SLE-Module-Basesystem15-Debuginfo-Updates SLE-Module-Basesystem15-Pool SLE-Module-Basesystem15-Source-Pool SLE-Module-Basesystem15-Updates SLE-Module-Containers15-Debuginfo-Pool SLE-Module-Containers15-Debuginfo-Updates SLE-Module-Containers15-Pool SLE-Module-Containers15-Source-Pool SLE-Module-Containers15-Updates SLE-Module-Desktop-Applications15-Debuginfo-Pool SLE-Module-Desktop-Applications15-Debuginfo-Updates SLE-Module-Desktop-Applications15-Pool \
     SLE-Module-Desktop-Applications15-Source-Pool SLE-Module-Desktop-Applications15-Updates SLE-Module-DevTools15-Debuginfo-Pool SLE-Module-DevTools15-Debuginfo-Updates SLE-Module-DevTools15-Pool SLE-Module-DevTools15-Source-Pool SLE-Module-DevTools15-Updates SLES15-15-0 SLE-Product-SLES15-Debuginfo-Pool SLE-Product-SLES15-Debuginfo-Updates SLE-Product-SLES15-Pool SLE-Product-SLES15-Source-Pool SLE-Product-SLES15-Updates SLE-Module-Packagehub-Subpackages15-Debuginfo-Pool SLE-Module-Packagehub-Subpackages15-Debuginfo-Updates
 SLE-Module-Packagehub-Subpackages15-Pool SLE-Module-Packagehub-Subpackages15-Source-Pool SLE-Module-Packagehub-Subpackages15-Updates SUSE-PackageHub-15-Debuginfo SUSE-PackageHub-15-Pool SUSE-PackageHub-15-Standard-Pool SLE-Module-Server-Applications15-Debuginfo-Pool SLE-Module-Server-Applications15-Debuginfo-Updates SLE-Module-Server-Applications15-Pool SLE-Module-Server-Applications15-Source-Pool SLE-Module-Server-Applications15-Updates SLE-Module-Web-Scripting15-Debuginfo-Pool SLE-Module-Web-Scripting15-Debuginfo-Updates SLE-Module-Web-Scripting15-Pool SLE-Module-Web-Scripting15-Source-Pool SLE-Module-Web-Scripting15-Updates
 
-cp oss.repo ${target}/etc/zypp/repos.d/.
+#cp oss.repo ${target}/etc/zypp/repos.d/.
 
 zypper -c ${zypp_config} --installroot=${target} --releasever=/ \
-    --setopt=tsflags=nodocs \
-    --setopt=group_package_types=mandatory -y install \
     --enablerepo=oss --nogpgcheck \
     sna-packages 
 
