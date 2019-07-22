a9b96720f5ae7bd79e6ecfb45cdb8c680d86252d
Vedarth <vedarth.sharma@gmail.com>
2019-07-22 22:03:28 +0530

    Clean vm

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 original_path=$(pwd)
-declare -a blacklist=(base/ microclimate/ origin/ akka/ earthquake/ react/ AMHub_Files/ websphere/)
+declare -a blacklist=(base/ microclimate/ origin/ akka/ earthquake/ react/ AMHub_Files/)
 
 containsElement () {
   local e match="$1"
@@ -14,6 +14,11 @@ do
     if [ $(containsElement $x "${blacklist[@]}" ; echo $?) == 1 ]
     then
         echo $x
-        cd $x ; make all ; cd $original_path
+        if [ $x == websphere/ ]
+        then
+            cd $x ; make ; cd $original_path
+        else
+            cd $x ; make all ; cd $original_path
+        fi
     fi
 done
