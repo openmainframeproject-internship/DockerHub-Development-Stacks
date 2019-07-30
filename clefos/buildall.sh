11e3d79f950c08722f4e47818242495b9dd95d00
Vedarth <vedarth.sharma@gmail.com>
2019-07-31 01:25:47 +0530

    Update paths

--- clefos/buildall.sh
+++ clefos/buildall.sh
@@ -14,11 +14,6 @@ do
     if [ $(containsElement $x "${blacklist[@]}" ; echo $?) == 1 ]
     then
         echo $x
-        if [ $x == websphere/ ]
-        then
-            cd $x ; make ; cd $original_path
-        else
-            cd $x ; make all ; cd $original_path
-        fi
+        cd $x ; make all ; cd $original_path
     fi
 done
